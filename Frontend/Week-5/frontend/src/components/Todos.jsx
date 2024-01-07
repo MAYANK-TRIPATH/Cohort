d6b46a19f0dd1dd47ddb2ea809d41427b4c9da62
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2024-01-07 11:11:41 +0530

    Todos add

--- Frontend/Week-5/frontend/src/components/Todos.jsx
+++ Frontend/Week-5/frontend/src/components/Todos.jsx
@@ -1,7 +1,12 @@
-export function Todos () {
-    return <div>
-        <h1>Go to gym</h1>
-        <h2>You need to go to the gym</h2>
-        <button>Mark as complete</button>
-    </div>
+export function Todos({todos}) {
+
+        return <div>
+        {todos.map(function(todo){
+            return <div>
+                <h1>{todo.title}</h1>
+                <h2>{todo.description}</h2>
+                <button>{todo.complete == true ? "Completed" : "Mark as Complete"}</button>
+            </div>
+        })}
+       </div> 
 }
