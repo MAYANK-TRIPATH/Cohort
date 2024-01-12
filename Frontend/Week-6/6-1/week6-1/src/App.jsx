938b61524151df2c67dd31c772457cefd28eea09
MAYANK-TRIPATH <mr.mayanktripathi2001@gmail.com>
2024-01-12 11:49:59 +0530

    key in React

--- Frontend/Week-6/6-1/week6-1/src/App.jsx
+++ Frontend/Week-6/6-1/week6-1/src/App.jsx
@@ -1,33 +1,79 @@
 import React, {Fragment} from "react";
 import { useState } from "react";
 
-function App() {
+// function App() {
 
+
+//   return (
+//     <div>
+//       <HeaderwithButton/>
+//       <Header title="Mayank2"></Header>
+//       <Header title="Mayank2"></Header>
+//     </div>
+//   )
+// }
+
+// function HeaderwithButton() {
+//   const [title, setTitle] = useState("My name is Mayank");
+//   function updateTitle() {
+//     setTitle("My name is " + Math.random());
+//   }
+
+//   return <div>
+//       <button onClick={updateTitle}>Update the title</button>
+//       <Header title={title}></Header>
+//   </div>
+// }
+
+// function Header({title}) {
+//   return <div>
+//     {title}
+//   </div>
+// }
+
+// -----------KEYS IN REACT-----------------------
+
+let counter = 4;
+
+function App() {
+  const [todos, setTodos] = useState([{
+    id: 1,
+    title: "go to gym",
+    description: "Go fast"
+  },{
+    id: 2,
+    title: "go to job",
+    description: "EArn money"
+  },{
+    id: 3,
+    title: "Have fun",
+    description: "Enjoy"
+  }])
+  function addTodo() {
+    setTodos([...todos, {
+      id: counter++,
+      title: Math.random(),
+      description: Math.random()
+    }])
+    }
+  
 
   return (
     <div>
-      <HeaderwithButton/>
-      <Header title="Mayank2"></Header>
-      <Header title="Mayank2"></Header>
+      <button onClick={addTodo}>Add a todo</button>
+      {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
     </div>
   )
 }
 
-function HeaderwithButton() {
-  const [title, setTitle] = useState("My name is Mayank");
-  function updateTitle() {
-    setTitle("My name is " + Math.random());
-  }
-
+function Todo({title, description}) {
   return <div>
-      <button onClick={updateTitle}>Update the title</button>
-      <Header title={title}></Header>
-  </div>
-}
-
-function Header({title}) {
-  return <div>
-    {title}
+    <h1>
+      {title}
+    </h1>
+    <h3>
+      {description}
+    </h3>
   </div>
 }
 
